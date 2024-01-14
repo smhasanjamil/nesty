5dbf780e58c59e3951fce28b94684c1ecf445c83
smhasanjamil <smhasanjamil23@gmail.com>
2024-01-14 16:08:46 +0600

    nextjs config file modify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [
       'res.cloudinary.com', 
@@ -12,4 +9,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
