4e085b10c2ff43857f4729b6f2ed4e2a599dd29e
smhasanjamil <smhasanjamil23@gmail.com>
2024-01-14 16:29:04 +0600

    update version to latest version

--- app/api/listings/[listingId]/route.ts
+++ app/api/listings/[listingId]/route.ts
@@ -32,3 +32,4 @@ export async function DELETE(
 
   return NextResponse.json(listing);
 }
+
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: [
       'res.cloudinary.com', 
@@ -9,4 +12,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
