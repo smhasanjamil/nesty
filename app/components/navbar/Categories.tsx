5ae62ea604c85598a0bf3d99ecc11c7394e74d36
smhasanjamil <smhasanjamil23@gmail.com>
2024-01-14 19:54:12 +0600

    Category and description

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { usePathname, useSearchParams } from "next/navigation";
-import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import { TbBeach, TbBeachOff, TbMountain, TbPool } from "react-icons/tb";
 import {
   GiBarn,
   GiBoatFishing,
   GiCactus,
+  GiCampingTent,
   GiCastle,
   GiCaveEntrance,
+  GiDesert,
+  GiFishingBoat,
   GiForestCamp,
+  GiGolfFlag,
   GiIsland,
+  GiModernCity,
+  GiVillage,
   GiWindmill,
 } from "react-icons/gi";
 import { FaSkiing } from "react-icons/fa";
@@ -17,84 +23,73 @@ import { BsSnow } from "react-icons/bs";
 import { IoDiamond } from "react-icons/io5";
 import { MdOutlineVilla } from "react-icons/md";
 
+import { FaBuildingFlag, FaTowerObservation } from "react-icons/fa6";
+import { HiHomeModern } from "react-icons/hi2";
+
+
 import Container from "../Container";
 import CategoryBox from "../CategoryBox";
 
 export const categories = [
   {
-    label: "Beach",
-    icon: TbBeach,
-    description: "This property is close to the beach!",
-  },
-  {
-    label: "Windmills",
-    icon: GiWindmill,
-    description: "This property is has windmills!",
-  },
-  {
-    label: "Modern",
-    icon: MdOutlineVilla,
-    description: "This property is modern!",
+    label: "Iconic cities",
+    icon: GiModernCity,
+    description: "Experience the vibrant energy of city life with this property located close to iconic landmarks!",
   },
   {
     label: "Countryside",
-    icon: TbMountain,
-    description: "This property is in the countryside!",
+    icon: GiVillage,
+    description: "Escape to the tranquility of the countryside with this property, offering a peaceful retreat away from the hustle and bustle.",
   },
   {
-    label: "Pools",
-    icon: TbPool,
-    description: "This is property has a beautiful pool!",
+    label: "Castles",
+    icon: FaBuildingFlag,
+    description: "Live like royalty in a majestic castle setting, surrounded by history and perhaps even windmills!",
+  },
+  {
+    label: "Tiny Homes",
+    icon: HiHomeModern,
+    description: "Embrace modern living with this chic and compact property, perfect for those who appreciate minimalist design.",
   },
   {
     label: "Islands",
     icon: GiIsland,
-    description: "This property is on an island!",
+    description: "Discover the charm of island life with this property, nestled in a serene island environment surrounded by nature.",
+  },
+  {
+    label: "Beach",
+    icon: TbBeachOff,
+    description: "Indulge in a beachside paradise with this property, featuring a stunning pool and easy access to the shoreline.",
+  },
+  {
+    label: "Camping",
+    icon: GiCampingTent,
+    description: "Immerse yourself in the beauty of nature on an island getaway with this camping-friendly property.",
   },
   {
     label: "Lake",
     icon: GiBoatFishing,
-    description: "This property is near a lake!",
+    description: "Experience lakeside living with this property, offering picturesque views and proximity to a tranquil lake.",
   },
   {
-    label: "Skiing",
-    icon: FaSkiing,
-    description: "This property has skiing activies!",
-  },
-  {
-    label: "Castles",
-    icon: GiCastle,
-    description: "This property is an ancient castle!",
-  },
-  {
-    label: "Caves",
-    icon: GiCaveEntrance,
-    description: "This property is in a spooky cave!",
-  },
-  {
-    label: "Camping",
-    icon: GiForestCamp,
-    description: "This property offers camping activities!",
-  },
-  {
-    label: "Arctic",
-    icon: BsSnow,
-    description: "This property is in arctic environment!",
+    label: "Golfing",
+    icon: GiGolfFlag,
+    description: "Enjoy a perfect blend of relaxation and recreational activities with this property, including skiing options.",
   },
   {
     label: "Desert",
-    icon: GiCactus,
-    description: "This property is in the desert!",
+    icon: GiDesert,
+    description: "Step into the enchanting world of an ancient castle surrounded by the mystique of the desert landscape.",
   },
   {
-    label: "Barns",
-    icon: GiBarn,
-    description: "This property is in a barn!",
+    label: "Towers",
+    icon: FaTowerObservation,
+    description: "Embark on an adventure with this property located in a tower within a mysterious and perhaps spooky cave setting.",
   },
   {
-    label: "Lux",
-    icon: IoDiamond,
-    description: "This property is brand new and luxurious!",
+    label: "Boats",
+    icon: GiFishingBoat,
+    description: "Combine the joys of camping and boating with this property, providing a unique waterfront experience.",
   },
 ];
 
@@ -109,6 +104,7 @@ const Categories = () => {
   }
 
   return (
+  
     <Container>
       <div
         className="
